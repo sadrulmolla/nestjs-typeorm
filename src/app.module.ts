b62b61de4a4eb58f33236d7ff18f5c5f12f623d3
slice-sadrul <sadrul.almmolla@sliceit.com>
2024-02-20 15:43:05 +0530

    feat: add everythinh in env variable

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import {TypeOrmModule} from "@nestjs/typeorm";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from '@nestjs/config';
 import { ClassicWarModule } from './module/classic-war/classic-war.module';
 import { PlayersModule } from './module/players/players.module';
@@ -7,14 +7,14 @@ import { getEntities } from './config/typeorm/entities-list';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({isGlobal: true}),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'sadrulmolla',
-      password: '',
-      database: 'coc',
+      host: process.env.POSTGRESQL_DB_HOST,
+      port: +process.env.POSTGRESQL_DB_PORT,
+      username: process.env.POSTGRESQL_DB_USERNAME,
+      password: process.env.POSTGRESQL_DB_PASSWORD,
+      database: process.env.POSTGRESQL_DB_NAME,
       entities: getEntities(),
       synchronize: true,
     }),
@@ -22,4 +22,4 @@ import { getEntities } from './config/typeorm/entities-list';
     PlayersModule,
   ],
 })
-export class AppModule {}
+export class AppModule { }
